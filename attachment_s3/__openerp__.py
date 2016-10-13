36028b73909bf78c236f2637a9bb22aac79220d8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-10-13 11:06:11 +0200

    Add attachment_s3

---
+++ attachment_s3/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{'name': 'Attachments on S3 storage',
+ 'summary': 'Store assets and attachments on a S3 compatible object storage',
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp',
+ 'license': 'AGPL-3',
+ 'category': 'Knowledge Management',
+ 'depends': ['base'],
+ 'external_dependencies': {
+    'python': ['boto'],
+ },
+ 'website': 'http://www.camptocamp.com',
+ 'data': [],
+ 'installable': True,
+ }
