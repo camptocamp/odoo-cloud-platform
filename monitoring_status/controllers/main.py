a0c926a478e9dc902525b15131faa363485cef5d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-01-03 14:25:20 +0100

    Expire sessions generated by health checks quickly
    The default expiration of sessions is 7 days. With healthchecks run
    every few seconds, we quickly have millions of anonymous sessions in
    Redis. Allow to define a custom expiration for some sessions and set a
    very short one for the monitoring requests.

--- monitoring_status/controllers/main.py
+++ monitoring_status/controllers/main.py
@@ -19,4 +19,13 @@ class Monitoring(http.Controller):
         # queue job, cron, database, ...
         headers = {'Content-Type': 'application/json'}
         info = {'status': 1}
+        session = http.request.session
+        # We set a custom expiration of 1 second for this request, as we do a
+        # lot of health checks, we don't want those anonymous sessions to be
+        # kept. Beware, it works only when session_redis is used.
+        # Alternatively, we could set 'session.should_save = False', which is
+        # tested in odoo source code, but we wouldn't check the health of
+        # Redis.
+        if not session.uid:
+            session.expiration = 1
         return werkzeug.wrappers.Response(json.dumps(info), headers=headers)
