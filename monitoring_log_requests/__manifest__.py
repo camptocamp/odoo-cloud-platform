535c658b70737654ed5f48ca11b28cca953d1d77
Jordi Riera <jordi.riera@numigi.com>
2019-02-11 17:46:04 -0500

    monitorin_log_request: _dispatch is a classmethod

--- monitoring_log_requests/__manifest__.py
+++ monitoring_log_requests/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
 {'name': 'Monitoring: Requests Logging',
- 'version': '11.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'version': '11.0.2.0.0',
+ 'author': 'Camptocamp,Numigi,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'category',
  'depends': ['base', 'web'],
