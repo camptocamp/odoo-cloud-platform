5492ca35514b2f412f2cb0ca7771b127825e1b9a
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-05-06 14:26:07 +0200

    Add monitoring_status

---
+++ monitoring_status/controllers/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import json
+
+import werkzeug
+
+from openerp import http
+from openerp.addons.web.controllers.main import ensure_db
+
+
+class Monitoring(http.Controller):
+
+    @http.route('/monitoring/status', type='http', auth='none')
+    def status(self):
+        ensure_db()
+        # TODO: add 'sub-systems' status and infos:
+        # queue job, cron, database, ...
+        headers = {'Content-Type': 'application/json'}
+        info = {'status': 1}
+        session = http.request.session
+        # We set a custom expiration of 1 second for this request, as we do a
+        # lot of health checks, we don't want those anonymous sessions to be
+        # kept. Beware, it works only when session_redis is used.
+        # Alternatively, we could set 'session.should_save = False', which is
+        # tested in odoo source code, but we wouldn't check the health of
+        # Redis.
+        if not session.uid:
+            session.expiration = 1
+        return werkzeug.wrappers.Response(json.dumps(info), headers=headers)
