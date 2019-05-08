c405f57cc264e59a9fc1857fcc5b7451e33b82c0
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-05-08 10:21:59 +0200

    Override dependencies with different name and pin versions
    The name of the libs and python packages are different, Odoo expects
    the inner python package in the manifest, but setuptools cannot find the
    libs in pypi, overrides them with the libs names.

--- setup/attachment_swift/setup.py
+++ setup/attachment_swift/setup.py
@@ -2,5 +2,13 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'swiftclient': 'python-swiftclient>=3.7.0',
+                'keystoneclient': 'python-keystoneclient>=3.19.0',
+                'keystoneauth1': 'keystoneauth1>=3.14.0',
+            },
+        },
+    }
 )
