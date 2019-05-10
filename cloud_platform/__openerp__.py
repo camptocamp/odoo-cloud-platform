60fa9669f6f96cb1fa0b1c55e7780d49e0b3f9d0
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-05-10 13:23:32 +0200

    Add cloud_platform

---
+++ cloud_platform/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{'name': 'Cloud Platform',
+ 'summary': 'Addons required for the Camptocamp Cloud Platform',
+ 'version': '7.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'category': 'Extra Tools',
+ 'depends': [
+     'base_attachment_object_storage',
+     'session_redis',
+     'monitoring_status',
+     'logging_json',
+     'server_environment',  # OCA/server-tools
+ ],
+ 'website': 'http://www.camptocamp.com',
+ 'data': [],
+ 'installable': True,
+ }
