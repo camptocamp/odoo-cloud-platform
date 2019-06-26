1af4f271df08844245f1565c4fb5904ccc18a6a1
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-06-26 14:42:50 +0200

    Fix copyright year

--- logging_json/__openerp__.py
+++ logging_json/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Camptocamp SA
+# Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'JSON Logging',
