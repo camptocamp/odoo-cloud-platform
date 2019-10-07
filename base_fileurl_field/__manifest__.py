b43a9b3594e2826af78fa262b9d460bf8c175903
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-10-07 16:48:48 +0200

    BSRD-286: MIG V13 base_fileurl_field

--- base_fileurl_field/__manifest__.py
+++ base_fileurl_field/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base FileURL Field",
     "summary": "Implementation of FileURL type fields",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Technical Settings",
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
@@ -11,5 +11,5 @@
         "base_attachment_object_storage",
     ],
     "auto_install": False,
-    "installable": False,
+    "installable": True,
 }
