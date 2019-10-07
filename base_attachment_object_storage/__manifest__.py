a7e364f93b2e2e76e1e2157fe2bbf47d95ce0c87
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-10-07 13:33:53 +0200

    [MIG] base_attachment_object_storage: Migration to 13.0

--- base_attachment_object_storage/__manifest__.py
+++ base_attachment_object_storage/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2017-2018 Camptocamp SA
+# Copyright 2017-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
 {'name': 'Base Attachment Object Store',
  'summary': 'Base module for the implementation of external object store.',
- 'version': '12.0.1.1.0',
+ 'version': '13.0.1.1.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Knowledge Management',
  'depends': ['base'],
  'website': 'http://www.camptocamp.com',
  'data': [],
- 'installable': False,
+ 'installable': True,
  'auto_install': True,
  }
