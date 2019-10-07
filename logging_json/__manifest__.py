c1c76144d2b1c21703146a4cf2385584be68daea
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-10-07 17:25:12 +0200

    BSRD-286: MIG V13 logging_json

--- logging_json/__manifest__.py
+++ logging_json/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2016-2018 Camptocamp SA
+# Copyright 2016-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'JSON Logging',
- 'version': '12.0.1.0.0',
+ 'version': '13.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Extra Tools',
@@ -13,5 +13,5 @@
  },
  'website': 'http://www.camptocamp.com',
  'data': [],
- 'installable': False,
+ 'installable': True,
  }
