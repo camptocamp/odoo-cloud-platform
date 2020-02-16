1ebaf89078114621838e34370931bb2cafda9adc
Russell Briggs <russell.briggs@junari.com>
2020-02-16 03:09:46 +0000

    Seperate http request fields in the log json

--- logging_json/json_log.py
+++ logging_json/json_log.py
@@ -8,6 +8,8 @@ import time
 
 from distutils.util import strtobool
 from odoo.netsvc import PerfFilter
+from odoo.service.server import RequestHandler
+from werkzeug.urls import uri_to_iri
 
 _logger = logging.getLogger(__name__)
 TIMING_DP = 6
@@ -24,10 +26,10 @@ def is_true(strval):
 
 
 class OdooJsonFormatter(jsonlogger.JsonFormatter):
-
     def add_fields(self, log_record, record, message_dict):
         record.pid = os.getpid()
         record.dbname = getattr(threading.currentThread(), 'dbname', '?')
+        # Remove perf_info - it is replaced by JsonPerfFilter fields, defined below
         if hasattr(record, "perf_info"):
             delattr(record, "perf_info")
         _super = super(OdooJsonFormatter, self)
@@ -35,10 +37,9 @@ class OdooJsonFormatter(jsonlogger.JsonFormatter):
 
 
 class JsonPerfFilter(logging.Filter):
-
     def filter(self, record):
         if hasattr(threading.current_thread(), "query_count"):
-            record.request_time = round(
+            record.response_time = round(
                 time.time() - threading.current_thread().perf_t0, TIMING_DP)
             record.query_count = threading.current_thread().query_count
             record.query_time = round(
@@ -48,6 +49,8 @@ class JsonPerfFilter(logging.Filter):
 
 
 if is_true(os.environ.get('ODOO_LOGGING_JSON')):
+
+    # Replace odoo default log formatter
     format = ('%(asctime)s %(pid)s %(levelname)s'
               '%(dbname)s %(name)s: %(message)s')
     formatter = OdooJsonFormatter(format)
@@ -61,3 +64,22 @@ if is_true(os.environ.get('ODOO_LOGGING_JSON')):
             http_logger.removeFilter(f)
     json_perf_filter = JsonPerfFilter()
     http_logger.addFilter(json_perf_filter)
+
+    # Configure http request logging
+    def log_request(self, code="-", size="-"):
+        try:
+            path = uri_to_iri(self.path)
+        except AttributeError:
+            # path isn't set if the requestline was bad
+            path = self.requestline
+        record = {
+            "method": self.command,
+            "path": path,
+            "http_ver": self.request_version,
+            "http_code": str(code),
+            "size": size,
+            "client_addr": self.address_string()
+        }
+        http_logger.info('request%s', '', extra=record)
+
+    RequestHandler.log_request = log_request
