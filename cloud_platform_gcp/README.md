2ac76a75f512b56ecfe2946e0eee7bb0a1acf8d3
Alexey Yushin <ayushin@apexlabs.ai>
2020-10-28 21:27:35 +0100

    initial commit

---
+++ cloud_platform_gcp/README.md
@@ -0,0 +1,6 @@
+Cloud Platform Google GCP
+=========================
+
+Install addons specific to the Google Cloud Platform setup.
+
+    * The object storage is S3 (Google Storage in interop mode)
---
+++ cloud_platform_gcp/models/cloud_platform.py
@@ -0,0 +1,45 @@
+# Copyright 2016-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import logging
+
+from odoo import models, api
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from odoo.addons.cloud_platform.models.cloud_platform import FilestoreKind
+    from odoo.addons.cloud_platform.models.cloud_platform import PlatformConfig
+except ImportError:
+    FilestoreKind = None
+    PlatformConfig = None
+    _logger.debug("Cannot 'import from cloud_platform'")
+
+
+class CloudPlatform(models.AbstractModel):
+    _inherit = 'cloud.platform'
+
+    @api.model
+    def _platform_kinds(self):
+        kinds = super(CloudPlatform, self)._platform_kinds()
+        kinds.append('gcp')
+        return kinds
+
+    @api.model
+    def _config_by_server_env_for_gcp(self):
+        configs = {
+            'prod': PlatformConfig(filestore=FilestoreKind.s3),
+            'integration': PlatformConfig(filestore=FilestoreKind.s3),
+            'labs': PlatformConfig(filestore=FilestoreKind.s3),
+            'test': PlatformConfig(filestore=FilestoreKind.db),
+            'dev': PlatformConfig(filestore=FilestoreKind.db),
+        }
+        return configs
+
+    # TODO: Assert that AWS_HOST is set to 'storage.googleapis.com'
+    # @api.model
+    # def check(self):
+
+    @api.model
+    def install_gcp(self):
+        self.install('gcp')
