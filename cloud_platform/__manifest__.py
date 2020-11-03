fee088f695d8e2507e38f3ac28b9d746b4643e5c
Patrick Tombez <patrick.tombez@camptocamp.com>
2020-11-03 15:58:32 +0100

    [MIG] cloud_platform: Migration to 14.0

--- cloud_platform/__manifest__.py
+++ cloud_platform/__manifest__.py
@@ -4,7 +4,7 @@
 
 {'name': 'Cloud Platform',
  'summary': 'Addons required for the Camptocamp Cloud Platform',
- 'version': '13.0.1.0.0',
+ 'version': "14.0.1.0.0",
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Extra Tools',
@@ -18,5 +18,5 @@
  ],
  'website': 'https://www.camptocamp.com',
  'data': [],
- 'installable': False,
+ 'installable': True,
  }
