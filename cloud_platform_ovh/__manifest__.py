2dfb5cad6cefb7a48cb7501074ee23164028c811
Patrick Tombez <patrick.tombez@camptocamp.com>
2020-11-03 15:58:32 +0100

    [MIG] cloud_platform_ovh: Migration to 14.0

--- cloud_platform_ovh/__manifest__.py
+++ cloud_platform_ovh/__manifest__.py
@@ -4,7 +4,7 @@
 
 {'name': 'Cloud Platform OVH',
  'summary': 'Addons required for the Camptocamp Cloud Platform on OVH',
- 'version': '13.0.1.0.0',
+ 'version': "14.0.1.0.0",
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Extra Tools',
@@ -14,5 +14,5 @@
  ],
  'website': 'https://www.camptocamp.com',
  'data': [],
- 'installable': False,
+ 'installable': True,
  }
