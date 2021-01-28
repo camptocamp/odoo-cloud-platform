160c707a289009c42fb33cad83f6af0386db4b34
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-01-27 21:43:07 -0600

    [FIX] attachment_s3: Formatting

--- attachment_s3/__manifest__.py
+++ attachment_s3/__manifest__.py
@@ -2,17 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-{'name': 'Attachments on S3 storage',
- 'summary': 'Store assets and attachments on a S3 compatible object storage',
- 'version': '12.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Knowledge Management',
- 'depends': ['base', 'base_attachment_object_storage'],
- 'external_dependencies': {
-     'python': ['boto3'],
- },
- 'website': 'https://www.camptocamp.com',
- 'data': [],
- 'installable': True,
- }
+{
+    "name": "Attachments on S3 storage",
+    "summary": "Store assets and attachments on a S3 compatible object storage",
+    "version": "12.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Knowledge Management",
+    "depends": ["base", "base_attachment_object_storage"],
+    "external_dependencies": {
+        "python": ["boto3"],
+    },
+    "website": "https://www.camptocamp.com",
+    "data": [],
+    "installable": True,
+}
--- attachment_s3/models/ir_attachment.py
+++ attachment_s3/models/ir_attachment.py
@@ -1,7 +1,5 @@
 # Copyright 2016-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-
-
 import base64
 import logging
 import os
@@ -27,7 +25,7 @@ class IrAttachment(models.Model):
     _inherit = "ir.attachment"
 
     def _get_stores(self):
-        l = ['s3']
+        l = ["s3"]
         l += super(IrAttachment, self)._get_stores()
         return l
 
@@ -46,11 +44,11 @@ class IrAttachment(models.Model):
         from the environment variable ``AWS_BUCKETNAME`` will be read.
 
         """
-        host = os.environ.get('AWS_HOST')
+        host = os.environ.get("AWS_HOST")
 
         # Ensure host is prefixed with a scheme (use https as default)
         if host and not urlsplit(host).scheme:
-            host = 'https://%s' % host
+            host = "https://%s" % host
 
         region_name = os.environ.get('AWS_REGION')
         access_key = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -58,24 +56,25 @@ class IrAttachment(models.Model):
         bucket_name = name or os.environ.get('AWS_BUCKETNAME')
 
         params = {
-            'aws_access_key_id': access_key,
-            'aws_secret_access_key': secret_key,
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key": secret_key,
         }
         if host:
-            params['endpoint_url'] = host
+            params["endpoint_url"] = host
         if region_name:
-            params['region_name'] = region_name
+            params["region_name"] = region_name
         if not (access_key and secret_key and bucket_name):
-            msg = _('If you want to read from the %s S3 bucket, the following '
-                    'environment variables must be set:\n'
-                    '* AWS_ACCESS_KEY_ID\n'
-                    '* AWS_SECRET_ACCESS_KEY\n'
-                    'If you want to write in the %s S3 bucket, this variable '
-                    'must be set as well:\n'
-                    '* AWS_BUCKETNAME\n'
-                    'Optionally, the S3 host can be changed with:\n'
-                    '* AWS_HOST\n'
-                    ) % (bucket_name, bucket_name)
+            msg = _(
+                "If you want to read from the %s S3 bucket, the following "
+                "environment variables must be set:\n"
+                "* AWS_ACCESS_KEY_ID\n"
+                "* AWS_SECRET_ACCESS_KEY\n"
+                "If you want to write in the %s S3 bucket, this variable "
+                "must be set as well:\n"
+                "* AWS_BUCKETNAME\n"
+                "Optionally, the S3 host can be changed with:\n"
+                "* AWS_HOST\n"
+            ) % (bucket_name, bucket_name)
 
             raise exceptions.UserError(msg)
         # try:
@@ -87,12 +86,12 @@ class IrAttachment(models.Model):
         except ClientError as e:
             # If a client error is thrown, then check that it was a 404 error.
             # If it was a 404 error, then the bucket does not exist.
-            error_code = e.response['Error']['Code']
-            if error_code == '404':
+            error_code = e.response["Error"]["Code"]
+            if error_code == "404":
                 exists = False
         except EndpointConnectionError as error:
             # log verbose error from s3, return short message for user
-            _logger.exception('Error during connection on S3')
+            _logger.exception("Error during connection on S3")
             raise exceptions.UserError(str(error))
 
         if not exists:
@@ -108,7 +107,7 @@ class IrAttachment(models.Model):
 
     @api.model
     def _store_file_read(self, fname, bin_size=False):
-        if fname.startswith('s3://'):
+        if fname.startswith("s3://"):
             s3uri = S3Uri(fname)
             try:
                 bucket = self._get_s3_bucket(name=s3uri.bucket())
@@ -116,12 +115,10 @@ class IrAttachment(models.Model):
                 _logger.exception(
                     "error reading attachment '%s' from object storage", fname
                 )
-                return ''
+                return ""
             try:
                 key = s3uri.item()
-                bucket.meta.client.head_object(
-                    Bucket=bucket.name,  Key=key
-                )
+                bucket.meta.client.head_object(Bucket=bucket.name, Key=key)
                 if bin_size:
                     return bucket.Object(key).content_length
                 with io.BytesIO() as res:
@@ -129,18 +126,16 @@ class IrAttachment(models.Model):
                     res.seek(0)
                     read = base64.b64encode(res.read())
             except ClientError:
-                read = ''
-                _logger.info(
-                    "attachment '%s' missing on object storage", fname
-                )
+                read = ""
+                _logger.info("attachment '%s' missing on object storage", fname)
             return read
         else:
             return super(IrAttachment, self)._store_file_read(fname, bin_size)
 
     @api.model
     def _store_file_write(self, key, bin_data):
-        location = self.env.context.get('storage_location') or self._storage()
-        if location == 's3':
+        location = self.env.context.get("storage_location") or self._storage()
+        if location == "s3":
             bucket = self._get_s3_bucket()
             obj = bucket.Object(key=key)
             with io.BytesIO() as file:
@@ -151,11 +146,9 @@ class IrAttachment(models.Model):
                     obj.upload_fileobj(file)
                 except ClientError as error:
                     # log verbose error from s3, return short message for user
-                    _logger.exception(
-                        'Error during storage of the file %s' % filename
-                    )
+                    _logger.exception("Error during storage of the file %s" % filename)
                     raise exceptions.UserError(
-                        _('The file could not be stored: %s') % str(error)
+                        _("The file could not be stored: %s") % str(error)
                     )
         else:
             _super = super(IrAttachment, self)
@@ -164,28 +157,22 @@ class IrAttachment(models.Model):
 
     @api.model
     def _store_file_delete(self, fname):
-        if fname.startswith('s3://'):
+        if fname.startswith("s3://"):
             s3uri = S3Uri(fname)
             bucket_name = s3uri.bucket()
             item_name = s3uri.item()
             # delete the file only if it is on the current configured bucket
             # otherwise, we might delete files used on a different environment
-            if bucket_name == os.environ.get('AWS_BUCKETNAME'):
+            if bucket_name == os.environ.get("AWS_BUCKETNAME"):
                 bucket = self._get_s3_bucket()
                 obj = bucket.Object(key=item_name)
                 try:
-                    bucket.meta.client.head_object(
-                        Bucket=bucket.name, Key=item_name
-                    )
+                    bucket.meta.client.head_object(Bucket=bucket.name, Key=item_name)
                     obj.delete()
-                    _logger.info(
-                        'file %s deleted on the object storage' % (fname,)
-                    )
+                    _logger.info("file %s deleted on the object storage" % (fname,))
                 except ClientError:
                     # log verbose error from s3, return short message for
                     # user
-                    _logger.exception(
-                        'Error during deletion of the file %s' % fname
-                    )
+                    _logger.exception("Error during deletion of the file %s" % fname)
         else:
             super(IrAttachment, self)._store_file_delete(fname)
