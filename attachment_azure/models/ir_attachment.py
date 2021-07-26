dfdc58d4d65aadd0c030db31693b42ff9f923d6a
Patrick Tombez <patrick.tombez@camptocamp.com>
2021-07-26 10:27:25 +0200

    [MIG] attachment_azure: Backport from 14.0 to 13.0

--- attachment_azure/models/ir_attachment.py
+++ attachment_azure/models/ir_attachment.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2019 Camptocamp SA
+# Copyright 2016-2021 Camptocamp SA
 # Copyright 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 import io
