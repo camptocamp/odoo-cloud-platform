94609a0ddcad7b513a66f2eef05e850b84227f37
Patrick Tombez <patrick.tombez@camptocamp.com>
2021-07-26 10:27:25 +0200

    [MIG] attachment_azure: Backport from 14.0 to 13.0

--- attachment_azure/__manifest__.py
+++ attachment_azure/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2019 Camptocamp SA
+# Copyright 2016-2021 Camptocamp SA
 # Copyright 2021 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Attachments on Azure storage",
     "summary": "Store assets and attachments on a Azure compatible object storage",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp, "
     "Open Source Integrators, "
     "Serpent Consulting Services, "
