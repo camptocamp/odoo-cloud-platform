11391132a33dfd4a1040f55b5818540daba792b4
Patrick Tombez <35060345+p-tombez@users.noreply.github.com>
2021-08-04 11:16:46 +0200

    [13.0][ADD] monitoring_prometheus (#239)
    [13.0][ADD] monitoring_prometheus

---
+++ monitoring_prometheus/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2016-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{
+    "name": "Monitoring: Prometheus Metrics",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "category",
+    "depends": [
+        "base",
+        "web",
+        "server_environment",
+    ],
+    "website": "http://www.camptocamp.com",
+    "data": [],
+    "external_dependencies": {
+        "python": ["prometheus_client"],
+    },
+    "installable": True,
+}
