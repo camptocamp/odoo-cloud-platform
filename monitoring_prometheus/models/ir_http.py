5ff0df3f750c4e7745028e47599b4d801c8f1a62
Patrick Tombez <35060345+p-tombez@users.noreply.github.com>
2021-08-04 11:16:46 +0200

    [13.0][ADD] monitoring_prometheus (#239)
    [13.0][ADD] monitoring_prometheus

---
+++ monitoring_prometheus/models/ir_http.py
@@ -0,0 +1,40 @@
+# Copyright 2016-2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+from odoo.http import request
+from prometheus_client import Summary, Counter
+
+
+REQUEST_TIME = Summary(
+    "request_latency_sec", "Request response time in sec", ["query_type"]
+)
+LONGPOLLING_COUNT = Counter("longpolling", "Longpolling request count")
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _dispatch(cls):
+        path_info = request.httprequest.environ.get("PATH_INFO")
+
+        if path_info.startswith("/longpolling/"):
+            LONGPOLLING_COUNT.inc()
+            return super()._dispatch()
+
+        if path_info.startswith("/metrics"):
+            return super()._dispatch()
+
+        if path_info.startswith("/web/static"):
+            label = "assets"
+        elif path_info.startswith("/web/content"):
+            label = "filestore"
+        else:
+            label = "client"
+
+        res = None
+        with REQUEST_TIME.labels(label).time():
+            res = super()._dispatch()
+
+        return res
