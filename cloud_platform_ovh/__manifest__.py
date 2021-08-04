9ce94caef9e0f85f8236eb192106aa729a786bae
Patrick Tombez <35060345+p-tombez@users.noreply.github.com>
2021-08-04 11:29:31 +0200

    [13.0][IMP] Make cloud_platform fully abstract + update related modules (#244)
    [13.0][IMP] Make cloud_platform fully abstract + update related modules

--- cloud_platform_ovh/__manifest__.py
+++ cloud_platform_ovh/__manifest__.py
@@ -2,17 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
-{'name': 'Cloud Platform OVH',
- 'summary': 'Addons required for the Camptocamp Cloud Platform on OVH',
- 'version': '13.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Extra Tools',
- 'depends': [
-     'cloud_platform',
-     'attachment_swift',
- ],
- 'website': 'https://www.camptocamp.com',
- 'data': [],
- 'installable': True,
- }
+{
+    "name": "Cloud Platform OVH",
+    "summary": "Addons required for the Camptocamp Cloud Platform on OVH",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "cloud_platform",
+        "attachment_swift",
+        "monitoring_statsd",
+    ],
+    "excludes": [
+        "cloud_platform_exoscale",
+    ],
+    "website": "https://www.camptocamp.com",
+    "data": [],
+    "installable": True,
+}
