3416431adc756de689f68d4ad28c9adc45442b00
Patrick Tombez <35060345+p-tombez@users.noreply.github.com>
2021-08-04 11:29:31 +0200

    [13.0][IMP] Make cloud_platform fully abstract + update related modules (#244)

--- cloud_platform/README.rst
+++ cloud_platform/README.rst
@@ -6,7 +6,7 @@ common to all platform providers.
 
 * Provide a quick install that we can call at the setup / migration
   of a database
-* Check if the environment variables are configured correctly according
-  to the instance's environment (prod, integration, test or dev) to prevent
+* Implements abstract methods to check if the environment variables are configured correctly
+  according to the instance's environment (prod, integration, test or dev) to prevent
   data corruption between the environments (such as the integration server
   writing on the production object storage).
--- cloud_platform_exoscale/__manifest__.py
+++ cloud_platform_exoscale/__manifest__.py
@@ -2,18 +2,22 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-
-{'name': 'Cloud Platform Exoscale',
- 'summary': 'Addons required for the Camptocamp Cloud Platform on Exoscale',
- 'version': '10.0.1.1.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'license': 'AGPL-3',
- 'category': 'Extra Tools',
- 'depends': [
-     'cloud_platform',
-     'attachment_s3',
- ],
- 'website': 'https://www.camptocamp.com',
- 'data': [],
- 'installable': True,
- }
+{
+    "name": "Cloud Platform Exoscale",
+    "summary": "Addons required for the Camptocamp Cloud Platform on Exoscale",
+    "version": "10.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        "cloud_platform",
+        "attachment_s3",
+        "monitoring_statsd",
+    ],
+    "excludes": [
+        "cloud_platform_ovh",
+    ],
+    "website": "https://www.camptocamp.com",
+    "data": [],
+    "installable": True,
+}
