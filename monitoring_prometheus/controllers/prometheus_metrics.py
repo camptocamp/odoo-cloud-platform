4223e926f8071ade787cfd7dda619a0e33562f33
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-11-03 10:03:34 +0100

    Backport monitoring_prometheus from 13.0

--- monitoring_prometheus/controllers/prometheus_metrics.py
+++ monitoring_prometheus/controllers/prometheus_metrics.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
 # Copyright 2016-2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import logging
 
 from odoo.http import Controller, route
-from prometheus_client import generate_latest
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from prometheus_client import generate_latest
+except (ImportError, IOError) as err:
+    _logger.warning(err)
 
 
 class PrometheusController(Controller):
