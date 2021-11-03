5b90629d8bd2d38d6c9eaa076a85ac5cc304e5f4
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-11-03 10:03:34 +0100

    Backport monitoring_promotheus from 13.0

--- monitoring_prometheus/__manifest__.py
+++ monitoring_prometheus/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Monitoring: Prometheus Metrics",
-    "version": "13.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "category",
