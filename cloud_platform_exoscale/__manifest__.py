90493e7310ecc4860f370cfd3632e385492b153a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-11-08 11:59:48 +0100

    Bump version after refactor

--- cloud_platform_exoscale/__manifest__.py
+++ cloud_platform_exoscale/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Cloud Platform Exoscale",
     "summary": "Addons required for the Camptocamp Cloud Platform on Exoscale",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Extra Tools",
--- cloud_platform_ovh/__manifest__.py
+++ cloud_platform_ovh/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Cloud Platform OVH",
     "summary": "Addons required for the Camptocamp Cloud Platform on OVH",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Extra Tools",
