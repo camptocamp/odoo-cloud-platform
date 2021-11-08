e8d4373cdb883dc8794c80c2af1bff5bbddc47ec
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-11-08 11:53:41 +0100

    Bump version after refactor

--- cloud_platform_ovh/__manifest__.py
+++ cloud_platform_ovh/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Cloud Platform OVH",
     "summary": "Addons required for the Camptocamp Cloud Platform on OVH",
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Extra Tools",
