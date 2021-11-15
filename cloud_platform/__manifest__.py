c9e63ee590787f42bb3214e0a3d5b8d7d5f9cda7
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2021-11-15 16:14:09 +0100

    Bump version

--- cloud_platform/__manifest__.py
+++ cloud_platform/__manifest__.py
@@ -5,7 +5,7 @@
 
 {'name': 'Cloud Platform',
  'summary': 'Addons required for the Camptocamp Cloud Platform',
- 'version': '11.0.1.0.0',
+ 'version': '11.0.2.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Extra Tools',
