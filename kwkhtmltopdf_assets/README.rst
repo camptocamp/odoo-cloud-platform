91e8999b5cc2d22b43869b91753d865ae7c39a2e
Vincent Renaville <vincent.renaville@camptocamp.com>
2022-12-12 10:31:16 +0100

    feat: add kwkhtmltopdf asset fix (#395)
    * feat: add kwkhtmltopdf asset fix

---
+++ kwkhtmltopdf_assets/README.rst
@@ -0,0 +1,10 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+============================
+Kwktnkltopdf Asset
+============================
+
+Force assets to be saved to be readable by kwkhtmltopdf server, the
+first time you print a report it failed to generate
+the pdf asset used for pdf
---
+++ kwkhtmltopdf_assets/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{
+    "name": "Kwkhtmltopdf: Asset fix",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "category",
+    "depends": [
+        "base",
+    ],
+    "website": "http://www.camptocamp.com",
+    "data": [],
+    "installable": True,
+}
