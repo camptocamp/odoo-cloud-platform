847ebb5ce5b14fec7f2b153385c3abb83350d06b
vrenaville <vincent.renaville@camptocamp.com>
2022-12-12 08:41:54 +0100

    feat: add kwkhtmltopdf asset fix

---
+++ kwkhtmltopdf_assets/README.rst
@@ -0,0 +1,8 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+
+============================
+Kwktnkltopdf Asset
+============================
+
+Force assets to be saved to be readable by kwkhtmltopdf server
---
+++ kwkhtmltopdf_assets/models/ir_qweb.py
@@ -0,0 +1,30 @@
+# Copyright 2016-2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo.tools import config
+from odoo import models
+
+
+class IrQweb(models.AbstractModel):
+
+    _inherit = "ir.qweb"
+
+    def _generate_asset_nodes_cache(
+        self,
+        bundle,
+        css=True,
+        js=True,
+        debug=False,
+        async_load=False,
+        defer_load=False,
+        lazy_load=False,
+        media=None,
+    ):
+        context_for_printing = self.env.context.copy()
+        if not config["test_enable"]:
+            context_for_printing["commit_assetsbundle"] = True
+        return super(
+            IrQweb, self.with_context(**context_for_printing)
+        )._generate_asset_nodes(
+            bundle, css, js, debug, async_load, defer_load, lazy_load, media
+        )
