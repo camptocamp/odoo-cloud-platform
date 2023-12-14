87b76dbd5bef56a2d1b9f751f8764ad4863a4bc9
Atte Isopuro <atte.isopuro@avoin.systems>
2023-12-14 11:05:13 +0200

    Partially revert "feat: remove unmaintened modules (#443)"
    We want to maintain a fork with attachment_s3.
    
    This reverts commit b1434e34d0a3df805ae9a413b5a89d951a07188b.

---
+++ attachment_s3/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{
+    "name": "Attachments on S3 storage",
+    "summary": "Store assets and attachments on a S3 compatible object storage",
+    "version": "15.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Knowledge Management",
+    "depends": ["base", "base_attachment_object_storage"],
+    "external_dependencies": {
+        "python": ["boto3"],
+    },
+    "website": "https://github.com/camptocamp/odoo-cloud-platform",
+    "data": [],
+    "installable": False,
+}
