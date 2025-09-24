843ea9484ce669e5b0178a02de89f5acfcb8d617
Andrés Zacchino <az@adhoc.inc>
2025-09-24 15:24:46 +0000

    logging_json: enhance logging output by splitting levels into stdout and stderr

--- logging_json/json_log.py
+++ logging_json/json_log.py
@@ -3,6 +3,7 @@
 
 import logging
 import os
+import sys
 import threading
 import uuid
 
@@ -34,11 +35,40 @@ class OdooJsonFormatter(JsonFormatter):
 
 
 if is_true(os.environ.get("ODOO_LOGGING_JSON")):
-    formatted_message = (
-        "%(asctime)s %(pid)s %(levelname)s %(dbname)s %(name)s: %(message)s"
-    )
+    formatted_message = "%(asctime)s %(pid)s %(levelname)s %(dbname)s %(name)s: %(message)s"
     formatter = OdooJsonFormatter(formatted_message)
-    logging.getLogger().handlers[0].formatter = formatter
+
+    if is_true(os.environ.get("ODOO_LOGGING_JSON_STDERR")):
+
+        class MaxLevelFilter(logging.Filter):
+            def __init__(self, max_level):
+                self.max_level = max_level
+
+            def filter(self, record):
+                return record.levelno < self.max_level
+
+        # keep the original level
+        root_logger = logging.getLogger()
+        original_level = root_logger.level
+
+        # Split lower levels into stdout
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.NOTSET)
+        stdout_handler.addFilter(MaxLevelFilter(logging.WARNING))
+        stdout_handler.setFormatter(formatter)
+
+        # Split WARNING and upper into stderr
+        stderr_handler = logging.StreamHandler(sys.stderr)
+        stderr_handler.setLevel(logging.WARNING)
+        stderr_handler.setFormatter(formatter)
+
+        # Replace handlers
+        root_logger.handlers = []
+        root_logger.setLevel(original_level)
+        root_logger.addHandler(stdout_handler)
+        root_logger.addHandler(stderr_handler)
+    else:
+        logging.getLogger().handlers[0].formatter = formatter
 
 
 # monkey patch Request constructor to store request_uuid
