0d92787a6ea30ff06211e369a6e3c07d9fc24f70
Don Kendall <kendall@donkendall.com>
2026-03-26 15:10:27 -0400

    [18.0][MIG] monitoring_log_requests: Migration to 18.0

--- monitoring_log_requests/__manifest__.py
+++ monitoring_log_requests/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Monitoring: Requests Logging",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp,Numigi,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "category",
