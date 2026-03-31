b86a32f8d559811c9dbf085a80faf26a7defb847
Maksym Yankin <yankinmk@gmail.com>
2026-03-31 09:07:45 +0300

    [REF] cloud_platform: server_environment should not be a required dependency

--- cloud_platform/__manifest__.py
+++ cloud_platform/__manifest__.py
@@ -5,17 +5,11 @@
 {
     "name": "Cloud Platform",
     "summary": "Addons required for the Camptocamp Cloud Platform",
-    "version": "16.0.2.0.0",
+    "version": "16.0.2.0.1",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Extra Tools",
-    "depends": [
-        "session_redis",
-        "monitoring_status",
-        "logging_json",
-        "server_environment",  # OCA/server-tools
-    ],
+    "depends": ["session_redis", "monitoring_status", "logging_json"],
     "website": "https://github.com/camptocamp/odoo-cloud-platform",
-    "data": [],
     "installable": True,
 }
