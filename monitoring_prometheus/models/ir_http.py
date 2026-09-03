12d19ee2bbb4e61fb8c0d577e5ae0825e07d1ed3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2026-09-03 15:03:17 +0200

    [ADD] monitoring_prometheus_queue_job
    This commit features a refactoring of monitoring_prometheus to get a
    cron gathering some metrics from the database, and then publishing the
    result on the /metrics endpoint
    
    The new module monitoring_prometheus_queue_job extends the cron to get
    information about the queue job states.

--- monitoring_prometheus/models/ir_http.py
+++ monitoring_prometheus/models/ir_http.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2021 Camptocamp SA
+# Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from prometheus_client import Counter, Summary
